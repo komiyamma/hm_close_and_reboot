65ea67838ff7fa3c7e3582c647db7eb92e3ad1e9
komiyamma <komiyama0629@gmail.com>
2024-12-27 11:39:45 +0900

    不要な記述のカット

--- test.bat
+++ test.bat
@@ -1,17 +1,8 @@
 rem このバッチファイルのディレクトリに
 pushd "%~dp0"
 
-rem timeout /t 1 /nobreak >nul
+rem 可能な限り安全に閉じる(特に常駐秀丸は強制的に閉じると、直後に安全に起動させられなくなるのでこの対処は重要）
 call HmWmClose.exe
 
-rem 常駐含め、全部全強制終了。
-rem taskkill /f /im hidemaru.exe
-
-rem (2) 常駐hidemaruが強制終了されてから、1秒以内に何か共用メモリ関連の解放が行われる？
-rem それを確実に実行させるためには、1秒なのか2秒なのか待機する必要があるっぽい。
-rem 次の行を rem でコメントアウトしてしまうとほぼ確実に共用メモリエラーが発生する。
-rem timeout /t 1 /nobreak >nul
-
-rem 秀丸のディレクトリに
 pushd "%~1"
 start hidemaru.exe /x"%~2" /a"show_release_note"
